9889219d3eff84d08531a02d78fa246de2dcbc94
Solomon Cammue <s.cammue@gmail.com>
2023-12-13 00:33:15 -0600

    added navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='text-white'>
+        <div className='max-w-[1240px] w-full h-20 px-4'>
+        <h1>Saloon</h1>
+            <div className='flex justify-between items-center'>
+           
+                <ul>
+                    <li>Home</li>
+                    <li>Beauty</li>
+                    <li>Galley</li>
+                    <li>Saloon</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
