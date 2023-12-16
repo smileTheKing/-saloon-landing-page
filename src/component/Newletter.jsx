0c4f64a7b9b1176dfeb4de35e69c2e2fd0d73adb
Solomon Cammue <s.cammue@gmail.com>
2023-12-16 16:59:10 -0600

    fix padding

--- src/component/Newletter.jsx
+++ src/component/Newletter.jsx
@@ -12,7 +12,7 @@ const Newletter = () => {
 
           <div className="flex flex-row h-[50px] border border-[#fff] w-full">
             <input type="text" placeholder="Enter you email" className="w-full bg-inherit px-2"/>
-            <input type="button" value='Subscribe' className="bg-white px-4 text-[#e14282] "/>
+            <input type="button" value='Subscribe' className="bg-white h-full px-4 text-[#e14282] "/>
           </div>
         </div>
       </div>
