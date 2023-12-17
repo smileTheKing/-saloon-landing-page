d853f684aa8235438f583b8079bec6418dbf6521
Solomon Cammue <s.cammue@gmail.com>
2023-12-16 20:31:40 -0600

    added input type

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -28,8 +28,8 @@ const Hero = () => {
           <div className="grid grid-cols-2 lg:grid-cols-4  gap-4">
             <input type="text" placeholder="name" className="border placeholder:text-center sm:w-56 p-1 border-[#4A4041]/30 rounded"/>
             <input type="number" placeholder="phone number" className="border placeholder:text-center sm:w-56 p-1 border-[#4A4041]/30 rounded"/>
-            <input type="time" className="border sm:w-56 p-1 border-[#4A4041]/30 text-gray-400 rounded " placeholder="choose time"  />
-            <input type="date"className="border sm:w-56 p-1 border-[#4A4041]/30 text-gray-400 rounded"placeholder="choose date" />
+            <input type="time" className="border w-full sm:w-56 p-1 border-[#4A4041]/30 text-gray-400 rounded " placeholder="choose time"  />
+            <input type="date"className="border w-full sm:w-56 p-1 border-[#4A4041]/30 text-gray-400 rounded"placeholder="choose date" />
           </div>
           <button className="text-white bg-[#e14282] px-12 py-2 lg:w-96 capitalize rounded ">Book an Appointment</button>
         </div>
