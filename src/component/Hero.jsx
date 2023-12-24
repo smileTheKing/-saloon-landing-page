56f613dd8a7af1393d72485497690183134c542b
Solomon Cammue <s.cammue@gmail.com>
2023-12-24 11:38:20 -0600

    added improve bg

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -64,11 +64,11 @@ const Hero = () => {
           // backgroundOrigin:'border-box',
          // backgroundPositionX:'500px'
         }}
-        className="bg-bottom h-[500px] md:bg-[center_right_-1rem] mx-auto w-full lg:h-screen  xl:bg-right bg-contain relative"
+        className="bg-bottom h-[500px] md:bg-[center_right_-2rem] lg:bg-[center_right_-2.5rem] mx-auto w-full lg:h-screen  bg-contain relative"
       >
         <div className="flex max-w-[1048px] mx-auto relative   flex-col  justify-center w-full h-full">
           <div className="absolute md:relative bottom-0 md:w-[60%] lg:w-[70%] w-full text-center md:text-start  min-h-1/2    grid   mt-24 md:-mt-0  lg:-mt-24 px-4">
-          <h1 className=" text-md py-4">Welcome To Your Luxury Beauty Experience</h1>
+          <h1 className=" text-lg py-4">Welcome To Your Luxury Beauty Experience</h1>
           <h1 className=" text-4xl break-words  font-bold md:text-5xl lg:text-6xl  xl:text-7xl capitalize font-serif  ">
             It's time to start your style with perfection
           </h1>
@@ -82,7 +82,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="w-full lg:w-[98%] xl:w-full mx-auto md:-bottom-[180px]  left-0 right-0 min-h-[240px] bg-white text-black place-items-center grid py-4 px-4 border-[1.5px] border-y-[#e14282]/50 ">
+      <div className="w-full lg:w-[75%]  mx-auto md:-bottom-[140px]  left-0 right-0 min-h-[240px] lg:absolute  bg-white text-black place-items-center grid py-4 px-4 border-[1.5px] border-y-[#e14282]/50 ">
           <h3 className="font-bold capitalize text-center text-[#e14282] text-2xl">
             Enhance Your Appearance At our salon
           </h3>
